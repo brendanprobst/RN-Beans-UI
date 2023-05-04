418117592bee1c2dac0f8fcdf960adc52db15db9
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-05-04 14:04:37 -0400

    fix: rebuilding

--- index.ts
+++ index.ts
@@ -12,31 +12,7 @@ import {
   Font,
 } from "./lib/types";
 import Text from "./lib/components/text";
-import Container from "./lib/components/container";
-import Icon from "./lib/components/icons";
-import Button from "./lib/components/button";
-import TextInput from "./lib/components/textInput";
-import RadioButton from "./lib/components/radioButton";
-import CheckBox from "./lib/components/checkBox";
-import Popover from "./lib/components/popover";
-import ModalWrapper from "./lib/components/modalWrapper";
-import ScreenFooter from "./lib/components/screenFooter";
+
 export type { BeansThemeType, ColorThemeType, FontThemeType, Font };
 
-export {
-  Text,
-  Container,
-  Icon,
-  Button,
-  TextInput,
-  RadioButton,
-  CheckBox,
-  Popover,
-  ModalWrapper,
-  ThemeContext,
-  BeansProvider,
-  useTheme,
-  useColors,
-  useFonts,
-  ScreenFooter,
-};
+export { Text, ThemeContext, BeansProvider, useTheme, useColors, useFonts };
